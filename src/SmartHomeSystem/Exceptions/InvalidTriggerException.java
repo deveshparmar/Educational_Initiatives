a7384174532b78b529ddcfa48382a03e6b603fbb
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2023-09-29 23:17:48 +0530

    added custom exceptions

---
+++ src/SmartHomeSystem/Exceptions/InvalidTriggerException.java
@@ -0,0 +1,7 @@
+package SmartHomeSystem.Exceptions;
+
+public class InvalidTriggerException extends Exception{
+    public InvalidTriggerException(String message) {
+        super(message);
+    }
+}
---
+++ src/SmartHomeSystem/Exceptions/UnsupportedActionException.java
@@ -0,0 +1,7 @@
+package SmartHomeSystem.Exceptions;
+
+public class UnsupportedActionException extends Exception{
+    public UnsupportedActionException(String message) {
+        super(message);
+    }
+}
