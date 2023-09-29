83a2bb89d54d94b448cb258ff8cbb8468f13d853
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2023-09-29 17:03:37 +0530

    Initial commit - Made device interface

---
+++ src/SmartHomeSystem/Device.java
@@ -0,0 +1,16 @@
+package SmartHomeSystem;
+
+/**
+ * The {@code Device} interface represents a smart home device that can be controlled.
+ * Implementing classes should provide methods to turn the device on, turn it off,
+ * and get the device's type.
+ */
+
+public interface Device {
+
+    void turnOn(int id);
+
+    void turnOff(int id);
+
+    String getDeviceType();
+}
