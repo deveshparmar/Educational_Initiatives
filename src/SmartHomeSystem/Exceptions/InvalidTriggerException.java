cbc259369655014726c1f7d145a0cad35c6409bd
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2023-10-05 00:19:28 +0530

    fixed bugs

--- src/SmartHomeSystem/Exceptions/InvalidTriggerException.java
+++ src/SmartHomeSystem/Exceptions/InvalidTriggerException.java
@@ -1,5 +1,8 @@
 package SmartHomeSystem.Exceptions;
 
+/**
+ * The {@code InvalidTriggerException} class represents custom exception for invalid trigger
+ */
 public class InvalidTriggerException extends Exception{
     public InvalidTriggerException(String message) {
         super(message);
--- src/SmartHomeSystem/Exceptions/UnsupportedActionException.java
+++ src/SmartHomeSystem/Exceptions/UnsupportedActionException.java
@@ -1,5 +1,8 @@
 package SmartHomeSystem.Exceptions;
 
+/**
+ * The {@code UnsupportedActionException} class represents custom exception for unsupported action
+ */
 public class UnsupportedActionException extends Exception{
     public UnsupportedActionException(String message) {
         super(message);
