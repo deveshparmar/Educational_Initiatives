a6e11ac8e4a2eaa5d378e14f86f9566defda91a3
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2023-10-05 23:05:24 +0530

    added comments

--- src/SmartHomeSystem/DeviceObserver.java
+++ src/SmartHomeSystem/DeviceObserver.java
@@ -1,5 +1,9 @@
 package SmartHomeSystem;
 
+/**
+ * The {@code DeviceObserver} interface represents an observer for smart home devices.
+ */
+
 public interface DeviceObserver {
     void update(String message);
 }
