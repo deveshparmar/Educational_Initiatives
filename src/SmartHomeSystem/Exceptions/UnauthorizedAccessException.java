7ebf2f21a55457eb598c36f2d2c2165f0af56906
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2023-10-05 23:00:54 +0530

    Update UnauthorizedAccessException.java

--- src/SmartHomeSystem/Exceptions/UnauthorizedAccessException.java
+++ src/SmartHomeSystem/Exceptions/UnauthorizedAccessException.java
@@ -1,5 +1,9 @@
 package SmartHomeSystem.Exceptions;
 
+/**
+ * The {@code InvalidTriggerException} class represents custom exception for unauthorized access
+ */
+
 public class UnauthorizedAccessException extends Exception{
     public UnauthorizedAccessException(String message) {
         super(message);
